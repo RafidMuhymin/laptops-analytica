d9e4f234e881765b42b7ee6900a5b9edac15d2e5
RafidMuhymin <rafidmuhymin@gmail.com>
2022-04-04 22:11:47 +0600

    added Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className="navbar d-flex p-3 justify-content-center gap-4">
+        <NavLink to={"/"}>HOME</NavLink>
+        <NavLink to={"/reviews"}>REVIEWS</NavLink>
+        <NavLink to={"/dashboard"}>DASHBOARD</NavLink>
+        <NavLink to={"/blog"}>BLOG</NavLink>
+        <NavLink to={"/about"}>ABOUT</NavLink>
+      </nav>
+    </header>
+  );
+}
