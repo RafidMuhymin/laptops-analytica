3012de0fcfb12e6105afb0ca2605516435072d52
RafidMuhymin <rafidmuhymin@gmail.com>
2022-04-04 22:18:16 +0600

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./components/About/About";
+import Blog from "./components/Blog/Blog";
+import Dashboard from "./components/Dashboard/Dashboard";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Reviews from "./components/Reviews/Reviews";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
