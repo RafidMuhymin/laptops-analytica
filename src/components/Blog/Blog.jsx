1175876cb4a4fe0d46279fb67ee71f1601d38f30
RafidMuhymin <rafidmuhymin@gmail.com>
2022-04-05 23:51:19 +0600

    added questions

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -3,36 +3,30 @@ export default function Blog() {
     <main>
       <h1 className="p-4 text-center">Questions & Answers</h1>
 
-      <div>
+      <div className="p-4">
         <h2>
           What is the <em>Context API</em> ?
         </h2>
 
         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
-          illum quis harum repellat labore rerum neque molestiae dignissimos
-          alias vitae?
+          The <em>Context API</em> is an API provided by React that allows
+          sharing data between a tree of components without having to pass them
+          via props manually at every level. The component tree can be very big
+          for a big project. In that case, Context API can be used to avoid
+          passing data via props again and again.
         </p>
 
         <h2>
-          What is <em>Semantic HTML</em> ?
+          What are <em>Semantic tags</em> ?
         </h2>
 
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ex hic
-          consectetur nihil repudiandae. Cumque exercitationem magni quo odio
-          minima.
-        </p>
-
-        <h2>
-          What is the difference between an <em>inline</em> and an
-          <em> inline block</em> element?
-        </h2>
-
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit dicta
-          odit iure, esse saepe sapiente blanditiis architecto pariatur nemo
-          impedit!
+          Semantic tags are HTML tags that allow writing semantic HTML or
+          meaningful HTML. For example, using the <code>main</code> tag for the
+          main content of a page, using <code>nav</code> for the navigations
+          areas, <code>aside </code>
+          for the sidebar, etc. A few other examples are, <code>h1</code>,
+          <code> section</code>,<code> figure</code>, etc.
         </p>
       </div>
     </main>
