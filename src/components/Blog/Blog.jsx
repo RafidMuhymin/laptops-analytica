b9a8ee7174509b337c3aff35a3162babe7c3bdc5
RafidMuhymin <rafidmuhymin@gmail.com>
2022-04-05 22:45:08 +0600

    added questions

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,3 +1,40 @@
 export default function Blog() {
-  return <h1>Blog</h1>;
+  return (
+    <main>
+      <h1 className="p-4 text-center">Questions & Answers</h1>
+
+      <div>
+        <h2>
+          What is the <em>Context API</em> ?
+        </h2>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+          illum quis harum repellat labore rerum neque molestiae dignissimos
+          alias vitae?
+        </p>
+
+        <h2>
+          What is <em>Semantic HTML</em> ?
+        </h2>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ex hic
+          consectetur nihil repudiandae. Cumque exercitationem magni quo odio
+          minima.
+        </p>
+
+        <h2>
+          What is the difference between an <em>inline</em> and an
+          <em> inline block</em> element?
+        </h2>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit dicta
+          odit iure, esse saepe sapiente blanditiis architecto pariatur nemo
+          impedit!
+        </p>
+      </div>
+    </main>
+  );
 }
