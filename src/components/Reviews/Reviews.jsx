bed76e1f5fd06764d123ccd0d86838d7d13d399b
RafidMuhymin <rafidmuhymin@gmail.com>
2022-04-05 21:58:59 +0600

    updated reviews style

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,6 +1,7 @@
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
 import useReviews from "../../hooks/useReviews";
+import "./Review.css";
 
 export default function Reviews({ count }) {
   const reviews = useReviews(count);
@@ -8,27 +9,36 @@ export default function Reviews({ count }) {
   return (
     <section>
       <h2 className="px-4 mt-4 fs-1 text-center">Customer Reviews</h2>
-      <div className="p-4">
+
+      <div className="p-4 d-flex flex-column gap-3">
         {reviews.map(({ id, name, image, rating, review }) => (
           <div
             key={id}
-            className="gap-4 mx-auto d-flex align-items-center"
+            className="review gap-4 mx-auto d-grid align-items-center justify-content-center"
             style={{
               maxWidth: "fit-content",
             }}
           >
-            <div className="p-2 gap-2 d-flex flex-column">
-              <img src={image} alt={name} className="rounded-circle" />
+            <div
+              className="p-2 gap-2 mx-auto d-flex flex-column align-items-center justify-content-center"
+              style={{ width: "200px" }}
+            >
+              <img
+                src={image}
+                alt={name}
+                className="rounded-circle"
+                width={128}
+              />
               <h3 className="text-center">{name}</h3>
             </div>
 
-            <h4>
+            <h4 className="text-center">
               {Array.from({ length: rating }).map(() => (
                 <Icon icon="emojione:star" />
               ))}
             </h4>
 
-            <p className="fs-5">{review}</p>
+            <p className="fs-4 text-center">{review}</p>
           </div>
         ))}
       </div>
