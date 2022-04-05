593231f94ebf3c77c46f4a8a776fd9c0b373f5f9
RafidMuhymin <rafidmuhymin@gmail.com>
2022-04-05 21:06:33 +0600

    added Reviews component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Intro from "./Intro/Intro";
+import Reviews from "./Reviews/Reviews";
 
 export default function Home() {
   return (
     <main>
       <Intro />
+      <Reviews />
     </main>
   );
 }
